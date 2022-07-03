572a04fb1a0f8ebcea79b3a26cb7b13797abd45e
Talison <talisonsantana76@gmail.com>
2022-07-03 16:12:50 -0300

    adicionando rotas

---
+++ backend/index.js
@@ -0,0 +1,31 @@
+require('dotenv').config();
+
+const express = require('express');
+
+// const tasksController = require('./controller/tasks.controller');
+
+const app = express();
+
+app.use(express.json());
+
+app.get('/tasks', (req, res) => {
+  console.log('entrou');
+});
+
+app.post('/tasks', (req, res) => {
+  console.log('entrou');
+});
+
+app.put('/tasks', (req, res) => {
+  console.log('entrou');
+});
+
+app.delete('/tasks', (req, res) => {
+  console.log('entrou');
+});
+
+const PORT = process.env.PORT || 3001;
+
+app.listen(PORT, () => {
+  console.log(`Escutando na porta ${PORT}`);
+});
